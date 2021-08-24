80e56a36e9b6177444b426eba0577fd3fa3213e5
Hisham Ahamad <hisham.ahamad@outlook.com>
2021-08-24 10:22:29 +0530

    Update foreground.js

--- foreground.js
+++ foreground.js
@@ -1,6 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
-    if (window.location.hostname === "www.espncricinfo.com" && window.location.pathname === "/") {
-       document.getElementsByClassName("home-page-wrapper")[0].replaceChildren({});
-       console.log("ESPNCricinfo feed is now hidden!");
-    }
+if (window.location.hostname === "www.espncricinfo.com" && window.location.pathname === "/") {
+    document.getElementsByClassName("home-page-wrapper")[0].replaceChildren({});
+    console.log("ESPNCricinfo feed is now hidden!");
 }
