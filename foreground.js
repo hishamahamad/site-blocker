951c5889aa62ff036d258a5e18771d71a157eb30
Hisham Ahamad <hisham.ahamad@outlook.com>
2021-08-24 10:42:26 +0530

    Update foreground.js

--- foreground.js
+++ foreground.js
@@ -1,6 +1,10 @@
 if (window.location.hostname === "www.espncricinfo.com" && window.location.pathname === "/") {
     const blankSection = document.createElement("div");
-    blankSection.setAttribute("style", "margin: 5rem");
-    document.getElementsByClassName("home-page-wrapper")[0].replaceChildren(blankSection);
+    const text = document.createTextNode("Your feed has been whitewashed. Get it? ;-P");
+    blankSection.setAttribute("style", "margin: 20rem");
+    blankSection.appendChild(text);
+    
+    const targetElement = document.getElementsByClassName("home-page-wrapper")[0];
+    targetElement.replaceChildren(blankSection);
     console.log("ESPNCricinfo feed is now hidden!");
 }
