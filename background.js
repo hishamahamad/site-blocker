3a5a54655b991afce79a6b7df4111fc18fc613c0
Hisham Ahamad <hisham.ahamad@outlook.com>
2021-08-24 09:32:07 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,6 +1,8 @@
 chrome.runtime.onInstalled.addListener(() => {
-  if (window.location.hostname === "www.espncricinfo.com" && window.location.pathname === "/") {
-     document.getElementsByClassName("home-page-wrapper")[0].replaceChildren({});
-     console.log("ESPNCricinfo feed is now hidden!");
+  document.addEventListener('DOMContentLoaded', function() {
+    if (window.location.hostname === "www.espncricinfo.com" && window.location.pathname === "/") {
+       document.getElementsByClassName("home-page-wrapper")[0].replaceChildren({});
+       console.log("ESPNCricinfo feed is now hidden!");
+    }
   }
 });
