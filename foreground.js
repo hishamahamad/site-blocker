ea3acba6666a4493bf6df8d0879a7d17d59964a9
Hisham Ahamad <hisham.ahamad@outlook.com>
2021-08-24 10:45:20 +0530

    Update foreground.js

--- foreground.js
+++ foreground.js
@@ -1,7 +1,7 @@
 if (window.location.hostname === "www.espncricinfo.com" && window.location.pathname === "/") {
     const blankSection = document.createElement("div");
     const text = document.createTextNode("Your feed has been whitewashed. Get it? ;-P");
-    blankSection.setAttribute("style", "margin: 20rem");
+    blankSection.setAttribute("style", "margin: 10rem; font-size: 3rem; text-align: center;");
     blankSection.appendChild(text);
     
     const targetElement = document.getElementsByClassName("home-page-wrapper")[0];
