624f765fcf192751b704d2d97b00509c4c3499a1
fxultra <hisham.ahamad@outlook.com>
2021-09-28 15:56:20 +0530

    Update foreground.js

--- foreground.js
+++ foreground.js
@@ -1,7 +1,7 @@
 // document.addEventListener('DOMContentLoaded', function() {
     if (window.location.hostname === "www.espncricinfo.com" && window.location.pathname === "/") {
         const blankSection = document.createElement("div");
-        const text = document.createTextNode("Your feed has been whitewashed. Get it? ;-P");
+        const text = document.createTextNode("This is for your own good");
         blankSection.setAttribute("style", "margin: 10rem; font-size: 3rem; text-align: center;");
         blankSection.appendChild(text);
 
