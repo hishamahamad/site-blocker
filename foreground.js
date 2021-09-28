c8d861bf08c9993a706aaf7c9beed76376370030
fxultra <hisham.ahamad@outlook.com>
2021-09-28 16:01:19 +0530

    Update foreground.js

--- foreground.js
+++ foreground.js
@@ -1,13 +1,13 @@
-// document.addEventListener('DOMContentLoaded', function() {
-    if (window.location.hostname === "www.espncricinfo.com") {
-        const blankSection = document.createElement("div");
-        const text = document.createTextNode("This is for your own good");
-        blankSection.setAttribute("style", "margin: 10rem; font-size: 3rem; text-align: center;");
-        blankSection.appendChild(text);
 
-        const targetElement = document.body;
-        targetElement.replaceChildren(blankSection);
-        document.title = "<redacted>";
-        console.log("ESPNCricinfo is blanked out!");
-    }
-// }, false);
+if (window.location.hostname === "www.espncricinfo.com") {
+    window.stop()
+    const blankSection = document.createElement("div");
+    const text = document.createTextNode("This is for your own good");
+    blankSection.setAttribute("style", "margin: 10rem; font-size: 3rem; text-align: center;");
+    blankSection.appendChild(text);
+
+    const targetElement = document.body;
+    targetElement.replaceChildren(blankSection);
+    document.title = "<redacted>";
+    console.log("ESPNCricinfo is blanked out!");
+}
