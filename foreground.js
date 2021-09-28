f0f6173967fc810eafcd653860799db470db098e
fxultra <hisham.ahamad@outlook.com>
2021-09-28 15:55:18 +0530

    Update foreground.js

--- foreground.js
+++ foreground.js
@@ -1,12 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
+// document.addEventListener('DOMContentLoaded', function() {
     if (window.location.hostname === "www.espncricinfo.com" && window.location.pathname === "/") {
         const blankSection = document.createElement("div");
         const text = document.createTextNode("Your feed has been whitewashed. Get it? ;-P");
         blankSection.setAttribute("style", "margin: 10rem; font-size: 3rem; text-align: center;");
         blankSection.appendChild(text);
 
-        const targetElement = document.getElementsByTagName("body")[0];
+        const targetElement = document.body;
         targetElement.replaceChildren(blankSection);
         console.log("ESPNCricinfo feed is now hidden!");
     }
-}, false);
+// }, false);
