5f7081dedce86288d1a8a9311614359614b5e5f8
hishamahamad <hisham.ahamad@outlook.com>
2022-05-07 19:24:18 +0530

    Add youtube to blocklist

--- foreground.js
+++ foreground.js
@@ -1,15 +1,8 @@
+const blockedSites = [
+    "www.espncricinfo.com",
+    "www.youtube.com"
+];
 
-if (window.location.hostname === "www.espncricinfo.com") {
-//     window.stop()
-//     const blankSection = document.createElement("div");
-//     const text = document.createTextNode("This is for your own good");
-//     blankSection.setAttribute("style", "margin: 10rem; font-size: 3rem; text-align: center;");
-//     blankSection.appendChild(text);
-
-//     const targetElement = document.body;
-//     targetElement.replaceChildren(blankSection);
-//     document.title = "<redacted>";
-//     console.log("ESPNCricinfo is blanked out!");
-    
+if (blockedSites.includes(window.location.hostname)) {    
     history.back();
 }
